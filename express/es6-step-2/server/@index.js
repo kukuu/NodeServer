57abe8fc5d3d0414c80955bc5d6669744237aa46
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-09-05 14:17:05 +0100

    Instantiating CRUD and server side JS

---
+++ express/es6-step-2/server/@index.js
@@ -0,0 +1,27 @@
+// Steps : Project Setup: Serve index.html with ExpressJS - 
+
+// 1. run :"npm init -y" for default settings for package.json
+
+// 2. Initialise git repository - "git init"
+
+// 1. Using es6 on server - Requires babel transpiler. Node does not understand import so install 
+//i.  "npm  i --save-dev babel-cli"
+// ii. in package json in the script block define server key to point to "babel-node server/index.js" 
+// iii. if you run the server command "npm run server" the app will fail
+//iv. in the root folder create .babelrc file in root folder and . inside it define an object  with an array of presets
+
+
+
+// 4. Create server folder in repository and we beginn to build index.js as entry file
+
+import express from 'express';
+
+// Initialise express
+let app = express();
+
+//we define route for all routes
+app.get('/*', (req, res) => {
+	res.send('Hello World')
+})
+
+app.listen(3010, () => console.log('Running on localhost:3010'));
