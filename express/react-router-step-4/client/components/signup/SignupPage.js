cfd891558a3587ed0ef3cdfa3601a02ff9946278
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-09-07 16:03:55 +0000

    Ingestion with React router, step-4

---
+++ express/react-router-step-4/client/components/signup/SignupPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SignupPage extends React.Component {
+  render() {
+    return (
+      <h6>Sign up to KUKUU on GitHub</h6>
+    );
+  }
+}
+
+export default SignupPage;
