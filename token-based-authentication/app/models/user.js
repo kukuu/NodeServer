0da44eba0c8b2be700bf7d0411f6faa240385d18
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-10-11 22:06:22 +0100

    Adding more context checks

--- token-based-authentication/app/models/user.js
+++ token-based-authentication/app/models/user.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// set up a mongoose model
+// set up a mongoose model. Required in server.js. See "Secure Backend API" application with extended validation and encryption
 module.exports = mongoose.model('User', new Schema({ 
 	name: String, 
 	password:  String, 
