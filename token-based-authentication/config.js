cc866dd7ae0a9dfca05095d5e69b02877daafe51
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2017-11-07 16:25:21 +0000

    Notes to consider
    This is a low level JWT application. For a deeper and complex authentication see https://github.com/kukuu/secure-restful-react-redux-web-app. Note some authentication configurations settings are removed/disabled for security purposes.

--- token-based-authentication/config.js
+++ token-based-authentication/config.js
@@ -1,6 +1,8 @@
 module.exports = {
-
+	//Best Practice- store these as environmental veriables and include the parameters here.
+	//the values may change depending on varied execution platforms
+	//create a .env file at the root level and instantiate the parameters
 	'secret': 'ilovescotchyscotch',
-	//'database': 'mongodb://noder:noderauth@proximus.modulusmongo.net:27017/so9pojyN'
+	//'database': 'mongodb://your-localhost-url'
 
 };
